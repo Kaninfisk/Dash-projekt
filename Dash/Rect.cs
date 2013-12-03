95f65b9cf8519717f23ee58edcd85f2851ae0278
npbhdk <npbhdk@gmail.com>
2013-12-03 20:02:35 +0100

    Collision opdatering

---
+++ Dash/Rect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dash
+{
+    public class Rect
+    {
+        private float width;
+        private float height;
+        private PointF position;
+        
+        public PointF Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        
+
+        public Rect(PointF position, int width, int height)
+        {
+            this.position = position;
+            this.width = width;
+            this.height = height;
+        }
+
+        public RectangleF HitBox(float x, float y)
+        {
+            return new RectangleF(x + position.X, y + position.Y, width, height);
+        }
+    }
+}
